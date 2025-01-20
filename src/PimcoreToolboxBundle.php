9e0c55fc46b8bba56dd2ba14bf40f36c79cf573a
Adam Gąsowski <gander@users.noreply.github.com>
2025-01-20 10:01:55 +0100

    🚀

---
+++ src/PimcoreToolboxBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace GanderTools\PimcoreToolbox;
+
+use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
+
+class PimcoreToolboxBundle extends AbstractPimcoreBundle
+{
+    use PackageVersionTrait;
+}
